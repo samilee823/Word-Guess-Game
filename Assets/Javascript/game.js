91d22e6dba7109e3f9b45c9e171dfe2bfd85c617
Samantha Lee <sammialicelee@yahoo.com>
2020-06-08 13:10:17 -0400

    moved script to game.js

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,46 +1,30 @@
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
+var answers = ["Doc", "Sleepy", "Grumpy", "Sneezy", "Dopey", "Happy", "Bashful", "Snow White"]
+let random = Math.floor(Math.random() * answers.length)
+let wordChosen = answers[random]
+  console.log(wordChosen)
 
-<script> 
-{/* /* // 1. Display current word with spaces for letters */}
 $(document).ready(function() {
-//    Magic Kingdom
+//    Doc
+    
    document.onkeyup = function(event) {
+        var userText = event.key;
+        var letter = userText.toLowerCase();
+        var count = 0; 
+        var incorrect = []; 
 
-        var letter = event.key.toLowerCase();
-        // var winNumber; 
-        var userText = $("user-text");
+    for (var j = 0; j < wordChosen.length; j++) {
+        if (wordChosen.charAt(j) === letter) {
+        $("#word").append(letter);
+        count ++; 
+         } else 
+        $("#letters").append(letter);
 
-
-        if (letter === "d") {
-          console.log("d")
-          $("#word").append("<br><hr>" +"<h1>" + userText + "</h1>");
-          userText.textContent = event.key;
-
-        }
-
-        if (letter === "o") {
-        
-        }
-
-        if (letter === "c") {
-         
-        } else {
-
-        }
-
-        $("#wins").append("<br><hr>" +"<h1>" +winNumber + "</h1>");
+  }
+    
+        // $("#wins").append("<h1>" +winNumber + "</h1>");
 
       };
 
 });
-{/* // 2. Hear the letter the user typed */}
-{/* // 3. If the letter is correct, display in current word  */}
-{/* //    * Else letter is incorrect, display in "letters already guessed" */}
-{/* // 4. Count down number of guesses remaining  */}
-{/* // 5. Display answer and picture when guesses are done  */}
-{/* // 6. New word displayed and clear out letters  clearQueue()*/}
-{/* // 7. Display win count */}
-
-</script>
